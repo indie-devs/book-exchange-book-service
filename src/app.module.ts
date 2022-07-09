e7c8bcf2d186a47620046604db434709a0993716
Duy Nguyen <duy.nguyen@favro.com>
2022-07-09 22:04:16 +0700

    Add config service and func to get port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AppConfigService } from './config/appConfigService';
 
-@Module({})
-export class AppModule {}
+@Module({
+    providers: [ConfigService, AppConfigService]
+})
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppConfigService } from './config/appConfigService';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+
+  const appConfigService  = app.get<AppConfigService>(AppConfigService);
+  await app.listen(appConfigService.port);
 }
 bootstrap();
