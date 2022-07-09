d6064c3d4c6a411f42f27fde6290df14262dcf78
Duy Nguyen <duy.nguyen@favro.com>
2022-07-09 22:44:11 +0700

    Add cache and class validator

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/appConfigService';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(new ValidationPipe())
   const appConfigService  = app.get<AppConfigService>(AppConfigService);
   await app.listen(appConfigService.port);
 }
